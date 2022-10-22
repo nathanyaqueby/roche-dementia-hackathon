ce5d5c934bd23a53ae1ecd620599f75348361dd6
Nathanya Queby Satriani <nathanyaqueby21@gmail.com>
2022-10-22 11:15:15 +0200

    fix camera bug

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import streamlit as st
 st.title("Webcam Live Feed")
 run = st.checkbox('Run')
 FRAME_WINDOW = st.image([])
-camera = cv2.VideoCapture(1)
+camera = cv2.VideoCapture(0)
 
 while run:
     _, frame = camera.read()
